cdd14067dd7a9ca44022a08c98020607c4c987d5
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-08-20 15:39:06 -0400

    Created the users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_192252) do
+ActiveRecord::Schema.define(version: 2020_08_20_193445) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2020_08_20_192252) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["project_id"], name: "index_tickets_on_project_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
