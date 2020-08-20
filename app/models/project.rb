f5d712a5aed68fc84ed4229107631c4994afa19f
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-08-20 16:20:29 -0400

    Created the project model and it's associations and validations.

---
+++ app/models/project.rb
@@ -0,0 +1,6 @@
+class Project < ApplicationRecord
+  has_many :tickets, dependent: :destroy
+
+  validates: :name, presence: true, length: { minimum: 3 }, uniqueness: true
+  validates: :description, presence: true
+end
