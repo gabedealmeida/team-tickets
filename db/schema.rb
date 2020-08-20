450663ecc2a1a2b10a6d7ac124630825ab94a5ad
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-08-20 15:47:35 -0400

    Created the migration for the tags table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_193939) do
+ActiveRecord::Schema.define(version: 2020_08_20_194528) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2020_08_20_193939) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
