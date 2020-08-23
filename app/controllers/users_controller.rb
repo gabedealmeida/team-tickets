9042a106d6b6e5b2d4abb8607a1fa9ba30b45cd9
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-08-23 16:45:34 -0400

    Created users controller and added functionality to create user.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to Ticket Teams, #{@user.username} 🥳!!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do that."
+      redirect_to root_path
+    end
+  end
+end
