c4afccc29004c41e9cbaad42f8c3455e92aa46d7
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-08-23 16:40:45 -0400

    Modified the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
 
-  resources :projects do
-    resources :tickets
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  resources :projects
+  resources :tickets do
+    resources :comments, except: [:index, :show, :new]
   end
+  resources :tags
+  resources :users, only: [:create, :edit, :update]
 end
