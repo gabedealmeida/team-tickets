01e30c92e27922f74f96a635f1229964816df17b
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-08-23 16:37:29 -0400

    Modified existing routes and added new ones to make the functionality better.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :ticket
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+
+  validates :body, presence: true
 end
